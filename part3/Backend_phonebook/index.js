5cbd37118dfd03dcb5a504d052f15a7eb0b723e9
strangka <strangka@student.21-school.ru>
2025-09-13 09:55:13 +0700

    -3.8

--- part3/Backend_phonebook/index.js
+++ part3/Backend_phonebook/index.js
@@ -1,115 +1,98 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { id: '1', name: 'Arto Hellas', number: '040-123456' },
+  { id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: '3', name: 'Dan Abramov', number: '12-43-234345' },
+  { id: '4', name: 'Mary Poppendieck', number: '39-23-6423122' }
 ]
 
 // logger
+morgan.token('body', (req) => JSON.stringify(req.body))
 
-morgan.token('body', (req) => {
-    return JSON.stringify(req.body)
-})
-
-app.use(morgan(function (tokens, req, res) {
+app.use(
+  morgan(function (tokens, req, res) {
     return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens.body(req, res)
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens.body(req, res)
     ].join(' ')
-}))
+  })
+)
 
-
-app.get('/api/persons/', (request, response) => {
-    response.json(persons).status(200).end()
+app.get('/', (req, res) => {
+  res.send('Phonebook backend is running!')
 })
 
-app.get('/info', (request, response) => {
-    const date = new Date()
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>`
-    ).status(200).end()
+app.get('/api/persons', (req, res) => {
+  res.status(200).json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.status(200).send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
 
-    if (person) {
-    response.status(200).json(person)
-    } else {
-        response.status(404).json({ error: `Person with id ${id} not found` })
-    }
-});
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find((p) => p.id === id)
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+  if (person) {
+    res.status(200).json(person)
+  } else {
+    res.status(404).json({ error: `Person with id ${id} not found` })
+  }
+})
 
-    response.status(204).end()
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter((p) => p.id !== id)
+  res.status(204).end()
 })
 
 const generateId = () => {
-    const maxId = persons.length > 0 
-    ? Math.max(...persons.map(p => Number(p.id)))
-    : 0
-
-    return String(maxId + 1);
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0
+  return String(maxId + 1)
 }
 
-app.post('/api/persons/', (request, response) => {
-    const body = request.body
-    if (!body.name) {
-        return response.status(400).json({
-            error: `name is missing`
-        })
-    } else if (!body.number) {
-        return response.status(400).json({
-            error: `number is missing`
-        }) 
-    } else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: `name must be unique`
-        })
-    }
+app.post('/api/persons', (req, res) => {
+  const body = req.body
 
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
+  if (!body.name) {
+    return res.status(400).json({ error: 'name is missing' })
+  }
+  if (!body.number) {
+    return res.status(400).json({ error: 'number is missing' })
+  }
+  if (persons.find((p) => p.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
 
-    persons = persons.concat(person)
-    response.status(201).json(person)
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  res.status(201).json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
