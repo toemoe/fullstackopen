7d74bbd10de81862b5dac1b12e90dc3ce684806f
strangka <strangka@student.21-school.ru>
2025-09-20 15:28:51 +0700

    4.19

--- part4/backend/controllers/blogs.js
+++ part4/backend/controllers/blogs.js
@@ -1,6 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  } else return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
@@ -25,7 +33,11 @@ blogsRouter.post('/', async (request, response, next) => {
     if (!title || !url) {
       return response.status(400).json({ error: 'title or url missing' })
     }
-    const user = await User.findOne({})
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
     if (!user) {
       return response.status(400).json({ error: 'no user found' })
     }
