343d9931b57a374ba21de72173740205e3f2db01
strangka <strangka@student.21-school.ru>
2025-09-22 15:51:57 +0700

    fix

--- part4/backend/utils/config.js
+++ part4/backend/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV
+const MONGODB_URI = process.env.MONGODB_URI
 const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI
 
 module.exports = { MONGODB_URI, TEST_MONGODB_URI, PORT }
