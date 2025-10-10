7c29618940b2b470a2e4877268706062ad813cb2
strangka <strangka@student.21-school.ru>
2025-10-10 23:49:10 +0700

    7.8

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+  const onChange = (event) => { setValue(event.target.value) }
+  return { type, value, onChange }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(baseUrl)
+        setResources(response.data)
+      } catch (error) { console.error('Failed', error) }
+    }
+
+    fetchData()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    try {
+      const response = await axios.post(baseUrl, resource)
+      setResources(prev => prev.concat(response.data))
+      return response.data
+    } catch (error) { console.error('Failed to create', error) }
+  }
+
+  const service = { create }
+  return [ resources, service ]
+}
