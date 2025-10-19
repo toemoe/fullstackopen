4c27519d43f3f41367b21d73c6a2151200b044d9
strangka <strangka@student.21-school.ru>
2025-10-19 14:20:00 +0700

    8.13-8.14

--- part8/Blogs/library-backend.js
+++ part8/Blogs/library-backend.js
@@ -1,96 +1,37 @@
-const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import Author from './library-schema/author-schema.js'
+import Book from './library-schema/book-schema.js'
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+dotenv.config()
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+mongoose.set('strictQuery', false)
+const MONGODB_URI = process.env.MONGODB_URI
 
+try {
+  await mongoose.connect(MONGODB_URI)
+  console.log('Connected to MongoDB')
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error.message)
+}
+
+// ----- GRAPHQL SCHEMA -----
 const typeDefs = `
   type Book {
     title: String!
-    author: String!
     published: Int!
+    author: Author!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
     name: String!
     born: Int
-    bookCount: Int!
+    bookCount: Int
+    id: ID!
   }
 
   type Query {
@@ -99,7 +40,7 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
-  
+
   type Mutation {
     addBook(
       title: String!
@@ -107,6 +48,7 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
     editAuthor(
       name: String!
       born: Int
@@ -116,46 +58,64 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      let filteredBooks = books
-      if (args.author) filteredBooks = filteredBooks.filter(book => book.author === args.author)
-      if (args.genre) filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
-      return filteredBooks
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      let filter = {}
+      if (args.author) {
+        const author = await Author.findOne({ name: args.author })
+        if (author) filter.author = author._id
+      }
+      if (args.genre) {
+        filter.genres = args.genre
+      }
+      return Book.find(filter).populate('author')
     },
-    allAuthors: () => authors,
+    allAuthors: async () => Author.find({})
   },
+
   Author: {
-    bookCount: (root) => books.filter(book => book.author === root.name).length 
+    bookCount: async (root) => Book.countDocuments({ author: root._id })
   },
+
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args }
-      books = books.concat(book)
-      return book
-    },
-    editAuthor: (root, args) => {
-      let updated = null
-      authors = authors.map(author => {
-        if (author.name === args.name) {
-          updated = { ...author, born: args.born }
-          return updated
-        }
-        return author
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+
+      if (!author) {
+        author = new Author({ name: args.author })
+        await author.save()
+      }
+
+      const book = new Book({
+        title: args.title,
+        author: author._id,
+        published: args.published,
+        genres: args.genres
       })
-      return updated
+
+      await book.save()
+      return book.populate('author')
+    },
+
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
+      if (!author) return null
+
+      author.born = args.born
+      await author.save()
+      return author
     }
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 })
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
 })
+
+console.log(`Server ready at ${url}`)
---
+++ part8/Blogs/library-schema/book-schema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  genres: [
+    { type: String}
+  ]
+})
+
+schema.plugin(uniqueValidator)
+
+export default mongoose.model('Book', schema)
